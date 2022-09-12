be8182bd34c2f59c23c3aca4406da15c59c5240d
Huwaida Yafie <huwaidar5@gmail.com>
2022-09-12 20:19:47 +0700

    Latihan C++ Matkul Daspro

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "                  BIODATA DIRI" << endl;
+    cout << "==============================================" << endl;
+    cout << "Nama           : Huwaida Rahman Yafie" << endl;
+    cout << "NIM            : A11.2022.14687" << endl;
+    cout << "Alamat         : Banjarnegara, Jawa Tengah" << endl;
+    cout << "TTL            : Banjarnegara, 29 Mei 2004" << endl;
+    cout << "Hobi           : Menggambar" << endl;
+    cout << "==============================================" << endl;
+    return 0;
+}
